3bf0b0dbc07c91a4afd29be04e12d678939daf24
sjsnr <32189427+sjsnr@users.noreply.github.com>
2021-08-09 17:21:07 +0900

    [add] make function for complete task

--- src/index.js
+++ src/index.js
@@ -16,15 +16,33 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    // 押された完了ボタンの親タグをリストから削除
+    deleteFromIncompleteList(completeButton.parentNode);
+    // 完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+    // TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+    // div以下を初期化
+    addTarget.textContent = null;
+    // spanタグ生成
+    const span = document.createElement("span");
+    span.innerText = text;
+    // buttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    // liタグの子要素に各要素を設定
+    addTarget.appendChild(span);
+    addTarget.appendChild(backButton);
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // button（削除）タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    // 押された完了ボタンの親タグをリストから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   // ulタグの子要素のに各要素を設定
@@ -34,6 +52,11 @@ const onClickAdd = () => {
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
