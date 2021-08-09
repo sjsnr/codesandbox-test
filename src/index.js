ea85df06e990911cddf422f717132ca2c50ec632
sjsnr <32189427+sjsnr@users.noreply.github.com>
2021-08-09 17:29:41 +0900

    [add] make function for restore

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+  createIncompleteList(inputText);
+};
 
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // li生成
   const li = document.createElement("li");
 
   // spanタグ生成
   const span = document.createElement("span");
-  span.innerText = inputText;
+  span.innerText = text;
 
   // button（完了）タグ生成
   const completeButton = document.createElement("button");
@@ -30,6 +39,14 @@ const onClickAdd = () => {
     // buttonタグ生成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグを完了リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+      //　テキスト取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
     // liタグの子要素に各要素を設定
     addTarget.appendChild(span);
     addTarget.appendChild(backButton);
@@ -52,11 +69,6 @@ const onClickAdd = () => {
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li);
-};
-
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
