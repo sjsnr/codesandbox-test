6a9445cde7604c4efcf5451268e50af7f4b2eb9b
sjsnr <32189427+sjsnr@users.noreply.github.com>
2021-08-09 16:57:30 +0900

    [add] make function for delete task

--- src/index.js
+++ src/index.js
@@ -15,10 +15,17 @@ const onClickAdd = () => {
   // button（完了）タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
 
   // button（削除）タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
 
   // ulタグの子要素のに各要素を設定
   li.appendChild(span);
