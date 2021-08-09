6cb2c060a69ee7a9e2ba89576826c3046fb8b015
sjsnr <32189427+sjsnr@users.noreply.github.com>
2021-08-09 16:38:26 +0900

    [add]  add a new task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+
+  // spanタグ生成
+  const span = document.createElement("span");
+  span.innerText = inputText;
+
+  // ulタグの子要素のに各要素を設定
+  li.appendChild(span);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
